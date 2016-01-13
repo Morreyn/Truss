bdf7e684327c211af4fa79764d5b4840aa58f739
Misty Morreyn <misty.morreyn@gmail.com>
2016-01-12 21:23:04 -0800

    Cleaned a few comments

--- Truss/plugins/Scroll Progressbar/js/scroll progressbar.js
+++ Truss/plugins/Scroll Progressbar/js/scroll progressbar.js
@@ -6,12 +6,15 @@ Description: A stylized progressbar that shows your placement on the page.
 
 /*
 Primary objective: 
-    1. Show user's position in page/article by scroll progressbar.
-    2. Make sure scroll progressbar is unobtrusive.
+    1. Show user's position in page/article by scroll progressbar. <complete>
+    2. Scroll progressbar is unobtrusive. <complete>
     3. Scroll progressbar is adapted to tablet and mobile screens.
     
 Secondary objective: 
     1. When user selects scrollbar, go to where user selected on scrollbar within document.
+        Detect screen size
+        Create ratio of complete to screen size
+        
 */
 
 $(document).ready(function () {
@@ -19,9 +22,9 @@ $(document).ready(function () {
 
     var viewWidth = window.innerWidth,
         viewHeight = window.innerHeight,
-        bodyHeight = $('body').height(),
+        bodyHeight = $('body').height(), /*Change to identify a scrollProgressbar element*/
         mbh = bodyHeight - viewHeight,
-        maxlineheight = 506;
+        maxlineheight = 500;
 
     $(window).scroll(function (e) {
 
@@ -30,15 +33,40 @@ $(document).ready(function () {
 
         // Ratio of % scrolled 
         var y = ((st * maxlineheight) / mbh);
+
         var percentHidden = ((mbh - st) / mbh);
         percentHidden = Math.round(percentHidden * 10) / 10; /*Not quite working*/
-        $('.line').css('height', y);
+
+        $('.right-scroll-area .bar').css('height', y);
+        $('.top-scroll-area .bar').css('width', y);
 
         console.log(percentHidden);
         if (percentHidden = 0) {
-            $('.scroll-area').css("visibility", "hidden");
+            $('.top-scroll-area').css("visibility", "hidden");
         } else {
-            $('.scroll-area').css("visibility", "visible");
+            $('.top-scroll-area').css("visibility", "visible");
+        }
+    });
+
+    $(window).resize(function (e) {
+
+        /*scrollTop finds the number of pixels hidden from view above the scrollable area*/
+        var st = $('body').scrollTop();
+
+        // Ratio of % scrolled 
+        var y = ((st * maxlineheight) / mbh);
+
+        var percentHidden = ((mbh - st) / mbh);
+        percentHidden = Math.round(percentHidden * 10) / 10; /*Not quite working*/
+
+        $('.right-scroll-area .bar').css('height', y);
+        $('.top-scroll-area .bar').css('width', y);
+
+        console.log(percentHidden);
+        if (percentHidden = 0) {
+            $('.top-scroll-area').css("visibility", "hidden");
+        } else {
+            $('.top-scroll-area').css("visibility", "visible");
         }
     });
 });
